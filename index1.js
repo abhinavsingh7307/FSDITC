4e4a74e1bcd3f713836c2797421e1b6816db3d33
abhinavsingh7307 <abhinavsingh7529@gmail.com>
2025-09-22 13:01:06 +0530

    DOM file added

---
+++ index1.js
@@ -0,0 +1,25 @@
+/*const data=function(a,b){
+    return a*b
+}
+
+const data=msg->msg;
+(function(){
+
+})*/
+
+function selectLanguage(lang){
+    result=javaCompiler();
+}
+
+else if(lang=='c'){
+    function cCompiler(){
+    return "using c compiler"
+
+
+result=cCompiler();
+}
+else{
+    result="no compiler found";
+}
+return result;
+}
